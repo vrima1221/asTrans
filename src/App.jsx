35d4dca07f7c4a44f3810c5fa16f054f6d381c98
Vasya Rymarenko <vrima60@gmail.com>
2023-05-26 14:21:45 +0300

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.scss';
+import { Header } from './components/Header/Header';
 
 export const App = () => (
   <div className="starter">
     React starter pack
+    <Header />
   </div>
 );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => (
+  <h1 className="header">Header</h1>
+);
