2bbcd89447a312fb11d889ae11d91a47ebf5aae4
Vasya Rymarenko <vrima60@gmail.com>
2023-05-26 16:17:03 +0300

    basic background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { Header } from './components/Header/Header';
 
 export const App = () => (
   <div className="starter">
-    React starter pack
-    <Header />
+    <div className="wrapper">
+      <Header />
+    </div>
   </div>
 );
